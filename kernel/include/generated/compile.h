226fb41b3b010ecfc3eace919bdd94ade60a58ec
Kees Jongenburger <kees.jongenburger@gmail.com>
2014-08-26 10:15:04 +0200

    kernel cleanup after compare to https://github.com/bq/aquaris-5

--- kernel/include/generated/compile.h
+++
@@ -1,7 +0,0 @@
-/* This file is auto generated, version 2 */
-/* SMP PREEMPT */
-#define UTS_MACHINE "arm"
-#define UTS_VERSION "#2 SMP PREEMPT Fri Jul 26 11:17:28 CST 2013"
-#define LINUX_COMPILE_BY "mtk81316"
-#define LINUX_COMPILE_HOST "mbjswgbm539"
-#define LINUX_COMPILER "gcc version 4.6.x-google 20120106 (prerelease) (GCC) "
--- kernel/include/generated/utsrelease.h
+++
@@ -1,3 +0,0 @@
-#define UTS_RELEASE "3.4.5"
-#define BUILD_INFO "ALPS.JB2.MP.V1.16"
-#define BUILD_FINGERPRINT "ahong89_wet_jb2"
